bea1dfb0a2584913585009fbb72d57ccfcd149c2
Eon4 <ladyforest85@gmail.com>
2023-03-09 14:36:33 +0100

    update mickey
    mickey is up to date

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,71 +1,106 @@
 // script for disney api 
 
-// loading screen 
+let myPage = 1;
+const myAppElement = document.getElementById('myApp');
 
-let myPage=1;
-const myAppElement=document.getElementById('myApp');
-
-//entry point
+// entry point
 loadingScreen();
 setUpShowAllButton();
 setupSearchForm();
+fetchOneCharacter(4703);
 
-// loading screen kaldes når vi henter data 
+
+
+function fetchOneCharacter(myId) {
+    let URI = `https://api.disneyapi.dev/characters/${myId}`
+
+    fetch(URI).then((response) => {
+        //console.log(response);
+
+        if (response.ok) {
+            return response.json();
+        } else {
+            alert("api error du får lige mickey mouse");
+            fetchOneCharacter(4703);
+
+        }
+
+
+
+    }).then((data) => {
+
+        //console.log(data);
+        showCharacter(data);
+
+    }).catch();
+
+}
+
+function showCharacter(myData) {
+    //  myAppElement
+    console.log(myData.name);
+
+    let myFilms = '<h3>Films:</h3>';
+    myData.films.map((film) => {
+        myFilms += `${film}, `
+    }
+    );
+
+
+    let myTvShows = '<h3>TV Shows:</h3>';
+    myData.tvShows.map((show) => {
+        myTvShows += `${show}, `
+    }
+    );
+
+
+
+    let myHTML = `<h2>${myData.name}</h2><img src="${myData.imageUrl}"><p>${myFilms}</p><p>${myTvShows}</p>`;
+    myAppElement.innerHTML = myHTML;
+
+}
+
+// loading screen  kaldes når vi henter data
 function loadingScreen() {
     myAppElement.innerHTML = "<h2>Loading...</h2>";
+}
 
-    }
 
-    function setUpShowAllButton(){
-        let showAallButton = document.getElementById('showAllButton');
-        showAallButton.addEventListener('click',(e)=>{
-            myPage = 1;
-            fetchCharachterPage();
+function setUpShowAllButton() {
 
-        });
-    }
+    let showAllButton = document.getElementById('showAllButton');
+    showAllButton.addEventListener('click', (e) => {
+        myPage = 1;
+        fetchAllCharacters();
+    });
+}
 
-    function setupSearchForm() {
 
-        let searchButton = document.getElementById('searchButton');
 
-        searchButton.addEventListener('click',(e)=>{
-            e.preventDefault();
+function setupSearchForm() {
 
-            let searchInput = document.getElementById('searchInput');
-            let myValue = searchInput.value;
+    let searchButton = document.getElementById('searchButton');
 
-            if (myValue) {
-                console.log('vi har string');
-            }
-            else {
-                alert('indtast i søgefeltet');
-            }
+    searchButton.addEventListener('click', (e) => {
+        e.preventDefault();
 
-        });
+        let searchInput = document.getElementById('searchInput');
+        let myValue = searchInput.value;
 
-    }
+        if (myValue) {
+            console.log(' vi har string ' + myValue);
+        }
+        else {
+            alert('indtast i søge felt.');
+        }
 
-    const myCharachtersElement = document.getElementById('charachterSelect');
+    });
 
-    fetchCharachterPage("https://api.disneyapi.dev/characters");
+}
 
-    function fetchCharachterPage(chrachterId){
-        fetch(chrachterId)
-        .then (
-            (response) => {
-        // console.log('fetchCharachterPage');
-        return response.json();
-    }
-)
-.then((chrachterId) => {
-    console.log(`fetchCharachterPage:`, chrachterId);
-    setupCharachterSelection(chrachterId);
-})
-.catch((error) => {
-    console.log(error);
-});
-    }
+function fetchCharacterPage() {
+    console.log('fetchCharacterPage');
+}
 
     // function fetchCharachterPage(){
     //     console.log('fetchCharachterPage');
@@ -75,21 +110,3 @@ function loadingScreen() {
 
 
 
-// {
-//     "_id": 308,
-//     "films": ["Tangled", "Tangled: Before Ever After"],
-//     "shortFilms": ["Tangled Ever After", "Hare Peace"],
-//     "tvShows": ["Once Upon a Time", "Tangled: The Series"],
-//     "videoGames": [
-//       "Disney Princess Enchanting Storybooks",
-//       "Hidden Worlds",
-//       "Disney Crossy Road",
-//       "Kingdom Hearts III"
-//     ],
-//     "parkAttractions": ["Celebrate the Magic", "Jingle Bell, Jingle BAM!"],
-//     "allies": [],
-//     "enemies": [],
-//     "name": "Queen Arianna",
-//     "imageUrl": "https://static.wikia.nocookie.net/disney/images/1/15/Arianna_Tangled.jpg",
-//     "url": "https://api.disneyapi.dev/characters/308"
-//   }
